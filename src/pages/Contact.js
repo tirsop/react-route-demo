34370617067a6d12500f851863aaa8e02217ff97
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-23 19:22:49 +0900

    add possibility to add params

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,14 @@
+import { useLocation } from "react-router-dom"
+
 export default function Contact() {
+  const queryString = useLocation().search
+  console.log(queryString);
+  const queryParams = new URLSearchParams(queryString)
+  const name = queryParams.get("name")
+
   return (
     <div>
-      <h2>Contact</h2>
+      <h2>Hey {name}, Contact us here...</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro consequuntur dolor fugiat aliquid nisi possimus sapiente ab beatae laboriosam ducimus, inventore eum hic quisquam, provident accusamus exercitationem eos asperiores quibusdam!</p>
     </div>
   )
